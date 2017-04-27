35e86c68fba189bbc5914f62dbff72f127040e08
staunchjun1 <541084008@qq.com>
2017-04-27 20:15:48 +0800

    T63

--- src/T63.java
+++ src/T63.java
@@ -2,4 +2,32 @@
  * Created by Administrator on 2017/4/27.
  */
 public class T63 {
+public class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+    public TreeNode(int val) {
+        this.val = val;
+    }
+
+}
+    int k ;
+    TreeNode KthNode(TreeNode pRoot, int k)
+    {
+
+        this.k = k;
+        return  KthNodeCore(pRoot);
+
+    }
+    private  TreeNode KthNodeCore(TreeNode pRoot)
+    {
+        TreeNode target = null;
+        if (pRoot!=null )
+        {
+            if ((target = KthNodeCore(pRoot.left)) != null){return target;}
+            if (--k==0) {return pRoot;}
+            if ((target = KthNodeCore(pRoot.right)) != null){return target;}
+        }
+        return null;
+    }
 }
