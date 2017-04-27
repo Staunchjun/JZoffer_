3d3907d74d70c11647adccd7aa053514e908a0d4
staunchjun1 <541084008@qq.com>
2017-04-27 15:36:58 +0800

    t67

--- src/T67.java
+++ src/T67.java
@@ -2,4 +2,44 @@
  * Created by Administrator on 2017/4/27.
  */
 public class T67 {
+    public class TreeNode {
+        int val = 0;
+        TreeNode left = null;
+        TreeNode right = null;
+
+        public TreeNode(int val) {
+            this.val = val;
+
+        }
+
+        public int index = -1;
+
+        String Serialize(TreeNode root) {
+            StringBuffer sb = new StringBuffer();
+            if (root == null) {
+                sb.append("#,");
+                return sb.toString();
+            }
+            sb.append(root.val + ",");
+            sb.append(Serialize(root.left));
+            sb.append(Serialize(root.right));
+            return sb.toString();
+        }
+
+        TreeNode Deserialize(String str) {
+            index++;
+            int len = str.length();
+            if (index >= len) {
+                return null;
+            }
+            String[] chars = str.split(",");
+            TreeNode node = null;
+            if (!chars[index].equals("$")) {
+                node = new TreeNode(Integer.valueOf(chars[index]));
+                node.left = Deserialize(str);
+                node.right = Deserialize(str);
+            }
+            return node;
+        }
+    }
 }
