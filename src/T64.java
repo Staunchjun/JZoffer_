17c0ae33950cf88cf262fc1bc3217eadc7493906
staunchjun1 <541084008@qq.com>
2017-04-30 16:43:11 +0800

    T64

--- src/T64.java
+++ src/T64.java
@@ -1,5 +1,44 @@
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 /**
  * Created by Administrator on 2017/4/27.
  */
 public class T64 {
+    public class Solution {
+        private PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        private PriorityQueue<Integer> maxHeap = new PriorityQueue<>(15, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o2 - o1;
+            }
+        });
+        private int count = 0;
+        public void Insert(Integer num) {
+            if (count %2 == 0)
+            {
+                maxHeap.offer(num);
+                minHeap.offer(maxHeap.poll());
+            }else {
+                minHeap.offer(num);
+                maxHeap.offer(minHeap.poll());
+
+            }
+            count++;
+
+        }
+
+        public Double GetMedian() {
+            if (count %2 ==0)
+            {
+                return (maxHeap.peek()+minHeap.peek())/(2.0);
+            }
+            else
+            {
+                return new Double(minHeap.peek());
+            }
+        }
+
+
+    }
 }
