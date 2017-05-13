a9d0e33702a4ae3c6795e540dfcb6342e562f045
staunchjun1 <541084008@qq.com>
2017-05-13 21:31:11 +0800

    T34

---
+++ src/T35.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Administrator on 2017/5/13.
+ *在一个字符串(1<=字符串长度<=10000，
+ * 全部由大写字母组成)中找到第一个只出现一次的字符,并返回它的位置
+ */
+public class T35 {
+        public int FirstNotRepeatingChar(String str) {
+            return 0;
+        }
+}
