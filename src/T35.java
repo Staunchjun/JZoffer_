143f5e6e85da2b83ce8e60e58fe16cd5328198f8
staunchjun1 <541084008@qq.com>
2017-05-15 17:12:14 +0800

    T35

--- src/T35.java
+++ src/T35.java
@@ -1,3 +1,5 @@
+import java.util.LinkedHashMap;
+
 /**
  * Created by Administrator on 2017/5/13.
  *在一个字符串(1<=字符串长度<=10000，
@@ -5,6 +7,27 @@
  */
 public class T35 {
         public int FirstNotRepeatingChar(String str) {
-            return 0;
+            LinkedHashMap<Character,Integer> map = new LinkedHashMap<>();
+            for (int i = 0; i < str.length(); i++) {
+                char w = str.charAt(i);
+                if (map.containsKey(w))
+                {
+                    int count = map.get(w);
+                    map.put(w,++count);
+                }else {
+                    map.put(w,1);
+                }
+            }
+            int pos = -1;
+            for (int i = 0; i <str.length() ; i++) {
+                char w = str.charAt(i);
+                if (map.get(w) == 1)
+                {
+                    return i;
+                }
+
+            }
+
+            return pos;
         }
 }
