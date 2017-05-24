01c1e1eb8777b52657a4351c3f828b0c58d28e2b
staunchjun1 <541084008@qq.com>
2017-05-24 11:18:16 +0800

    Edit Distance (Modify)

--- src/EditDistance.java
+++ src/EditDistance.java
@@ -3,30 +3,32 @@
  */
 public class EditDistance {
     public static void main(String[] args) {
-        String a = "asdfsfa";
-        String b = "basdgref";
-        System.out.println("similarity:"+(1-(minDistance(a, b)*1.0/Math.max(a.length(),b.length()))));
-        System.out.println("distance:"+(minDistance(a, b)));
+        String a = "1,236,58,9,777";
+        String[] as = a.split(",");
+        String b = "1,236,57,100,9,877,777";
+        String[] bs = b.split(",");
+        System.out.println("similarity:"+(1-(minDistance(as, bs)*1.0/Math.max(as.length,bs.length))));
+        System.out.println("distance:"+(minDistance(as, bs)));
     }
 
-    public static int minDistance(String word1, String word2) {
-        if (word1.length() == 0) return word2.length();
-        if (word2.length() == 0) return word1.length();
+    public static int minDistance(String[] word1, String[] word2) {
+        if (word1.length == 0) return word2.length;
+        if (word2.length == 0) return word1.length;
 
-        int[][] distance = new int[word1.length() + 1][word2.length() + 1];
+        int[][] distance = new int[word1.length + 1][word2.length + 1];
 
-        for (int i = 0; i <= word1.length(); i++) {
+        for (int i = 0; i <= word1.length;i++) {
             distance[i][0] = i;
         }
 
-        for (int i = 0; i <= word2.length(); i++) {
+        for (int i = 0; i <= word2.length; i++) {
             distance[0][i] = i;
         }
 
-        for (int i = 1; i <= word1.length(); i++) {
-            for (int j = 1; j <= word2.length(); j++) {
+        for (int i = 1; i <= word1.length; i++) {
+            for (int j = 1; j <= word2.length; j++) {
                 int cost;
-                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                if (word1[i - 1].endsWith(word2[j - 1])) {
                      cost = 0;
                 } else {
                      cost = 1;
@@ -34,7 +36,7 @@ public class EditDistance {
                 distance[i][j] = min(distance[i - 1][j]+1, distance[i][j - 1]+1, distance[i - 1][j - 1]+ cost);
             }
         }
-        return distance[word1.length()][word2.length()];
+        return distance[word1.length][word2.length];
     }
 
     public static int min(int a, int b, int c) {
