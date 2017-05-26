458a51042efdeded444ff75e90a52c3980731f40
staunchjun1 <541084008@qq.com>
2017-05-26 09:53:52 +0800

    合并链表

--- src/T16.java
+++ src/T16.java
@@ -1,66 +1,31 @@
-import java.util.ArrayList;
-import java.util.Scanner;
-
 /**
  * Created by Administrator on 2017/3/20.输入两个单调递增的链表，输出两个链表合成后的链表，当然我们需要合成后的链表满足单调不减规则。
  */
 public class T16 {
+    public class ListNode {
+        int val;
+        ListNode next = null;
 
-    public  static int A = 1;
-    public static int B = 2;
-
-    public static boolean A_w = false;
-    public static boolean B_w = false;
-
-    public static void main(String[] args) {
-        Scanner scanner= new Scanner(System.in);
-        while (scanner.hasNext())
-        {
-             int n = scanner.nextInt();
-             if (n > 100)
-             {
-                 return;
-             }
-             else {
-                 for (int k = 1;k < n;k++)
-                 {
-                 takeStone(n,k,0,1);
-                 }
-             }
+        ListNode(int val) {
+            this.val = val;
         }
-
-
     }
-    public static void takeStone(int n, int t, int lastT, int who)
-    {
 
-
-        if(n == 0)
-        {
-            if (who == A)
-            {A_w= true;return;}
-            else
-            {B_w = true;return;}
+    public ListNode Merge(ListNode list1, ListNode list2) {
+        if (list1 == null) {
+            return list2;
+        } else if (list2 == null) {
+            return list1;
         }
-        if (t > n)
-        {
-            return;
+        ListNode merge2 = null;
+        if (list1.val > list2.val) {
+            merge2 = list2;
+            merge2.next = Merge(list1, list2.next);
+        } else {
+            merge2 = list1;
+            merge2.next = Merge(list1.next, list2);
         }
-        else if (lastT < t)
-        {
-            if (lastT != 0)
-            {
-            return;
-            }
-        }
-        int nRest = n - t;
-        lastT = t;
-        for (int k = 1;k < nRest && k < lastT ;k++)
-        {
-            if (who == A)
-            {takeStone(nRest,k,lastT,B);}
-            else
-            {takeStone(nRest,k,lastT,A);}
-        }
+        return merge2;
     }
 }
+
