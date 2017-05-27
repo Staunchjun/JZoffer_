940b8dda948c9c0650f0239e5b74932d3c44555e
staunchjun1 <541084008@qq.com>
2017-05-27 10:21:49 +0800

    镜像

--- src/T18.java
+++ src/T18.java
@@ -17,7 +17,18 @@ public class T18 {
 
     public class Solution {
         public void Mirror(TreeNode root) {
+                if (root == null)
+                {return;}
+                if (root.left == null && root.right == null)
+                {
+                    return;
+                }
+                TreeNode pTemp = root.left;
+                root.left = root.right;
+                root.right = pTemp;
 
+                Mirror(root.left);
+                Mirror(root.right);
         }
     }
 }
