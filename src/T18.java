98f84384fdbeb5eb99e8b15f631a24040f8a35ca
staunchjun1 <541084008@qq.com>
2017-05-30 15:45:31 +0800

    wangyi

--- src/T18.java
+++ src/T18.java
@@ -3,32 +3,32 @@
  */
 public class T18 {
 
-     public class TreeNode {
-     int val = 0;
-     TreeNode left = null;
-     TreeNode right = null;
+    public class TreeNode {
+        int val = 0;
+        TreeNode left = null;
+        TreeNode right = null;
 
-     public TreeNode(int val) {
-     this.val = val;
+        public TreeNode(int val) {
+            this.val = val;
 
-     }
+        }
 
-     }
+    }
 
     public class Solution {
         public void Mirror(TreeNode root) {
-                if (root == null)
-                {return;}
-                if (root.left == null && root.right == null)
-                {
-                    return;
-                }
-                TreeNode pTemp = root.left;
-                root.left = root.right;
-                root.right = pTemp;
+            if (root == null) {
+                return;
+            }
+            if (root.left == null && root.right == null) {
+                return;
+            }
+            TreeNode pTemp = root.left;
+            root.left = root.right;
+            root.right = pTemp;
 
-                Mirror(root.left);
-                Mirror(root.right);
+            Mirror(root.left);
+            Mirror(root.right);
         }
     }
 }
