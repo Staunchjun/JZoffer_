e8a4b36d7da0adffb109bc37f19714bb2e0b1588
staunchjun1 <541084008@qq.com>
2017-06-09 20:24:21 +0800

    Nw

--- src/NewCode/NK_3_2.java
+++ src/NewCode/NK_3_2.java
@@ -1,31 +1,64 @@
 package NewCode;
-
-import java.util.Scanner;
-
 /**
  * Created by Administrator on 2017/6/6.
  * 给出一个区间[a, b]，计算区间内“神奇数”的个数。
- 神奇数的定义：存在不同位置的两个数位，组成一个两位数（且不含前导0），
- 且这个两位数为质数。
- 比如：153，可以使用数字3和数字1组成13，13是质数，
- 满足神奇数。同样153可以找到31和53也为质数，只要找到一个质数即满足神奇数。
- 输入描述:
- 输入为两个整数a和b，代表[a, b]区间 (1 ≤ a ≤ b ≤ 10000)。
- 输出描述:
- 输出为一个整数，表示区间内满足条件的整数个数
- 输入例子:
- 11 20
- 输出例子:
- 6
+ * 神奇数的定义：存在不同位置的两个数位，组成一个两位数（且不含前导0），
+ * 且这个两位数为质数。
+ * 比如：153，可以使用数字3和数字1组成13，13是质数，
+ * 满足神奇数。同样153可以找到31和53也为质数，只要找到一个质数即满足神奇数。
+ * 输入描述:
+ * 输入为两个整数a和b，代表[a, b]区间 (1 ≤ a ≤ b ≤ 10000)。
+ * 输出描述:
+ * 输出为一个整数，表示区间内满足条件的整数个数
+ * 输入例子:
+ * 11 20
+ * 输出例子:
+ * 6
  */
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class NK_3_2 {
 
-    public static void main(String[] args)
-    {
-//        Scanner scanner = new Scanner(System.in);
-//        String nn = scanner.nextLine();
-//        String yy = scanner.nextLine();
-//        Kmeans k=new Kmeans(2);
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<Integer>();
+        for (int i = 11; i < 100; i++) {
+            boolean isPrime = true;
+            for (int j = 2; j <= Math.sqrt(i); j++) {
+                if (i % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) list.add(i);
+        }
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int cnt = 0;
+        for (int i = a; i <= b; i++) {
+            String s = String.valueOf(i);
+            for (Integer e : list) {
+                String s1 = String.valueOf(e / 10);
+                String s2 = String.valueOf(e % 10);
+                if (!s1.equals(s2)) {
+                    if (s.indexOf(s1) != -1 && s.indexOf(s2) != -1) {
+                        cnt++;
+                        break;
+                    }
+                } else {
+                    int index = s.indexOf(s1);
+                    int d = s.indexOf(s2, index+1);
+                    if (index != -1 && d  != -1) {
+                        cnt++;
+                        break;
+                    }
+                }
+            }
 
+        }
+        System.out.println(cnt);
     }
 }
--- src/NewCode/NK_3_4.java
+++ src/NewCode/NK_3_4.java
@@ -1,7 +1,47 @@
 package NewCode;
-
 /**
  * Created by Administrator on 2017/6/6.
+ * 牛牛有一个数组，里面的数可能不相等，
+ * 现在他想把数组变为：所有的数都相等。问是否可行。
+ * 牛牛可以进行的操作是：将数组中的任意一个数改为这个数的两倍。
+ * 这个操作的使用次数不限，也可以不使用，并且可以对同一个位置使用多次。
+ * 输入描述:
+ * 输入一个正整数N (N <= 50)
+ * 接下来一行输入N个正整数，每个数均小于等于1e9.
+ * 输出描述:
+ * 假如经过若干次操作可以使得N个数都相等，那么输出"YES", 否则输出"NO"
+ * 输入例子:
+ * 2
+ * 1 2
+ * 输出例子:
+ * YES
  */
+import java.util.Scanner;
 public class NK_3_4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int length = scanner.nextInt();
+        int[] nums = new int[length];
+        for (int i = 0; i < length; i++) {
+            nums[i] = scanner.nextInt();
+        }
+        for (int i = 0; i < length; i++) {
+            while (nums[i] % 2 == 0)
+                nums[i] = nums[i] / 2;
+        }
+        int basicNum = 0;
+        for (int i = 0; i < length; i++) {
+            if (i == 0) {
+                basicNum = nums[i];
+                continue;
+            }
+            if (basicNum != nums[i]) {
+                System.out.print("NO");
+                return;
+            } else {
+                continue;
+            }
+        }
+        System.out.print("YES");
+    }
 }
