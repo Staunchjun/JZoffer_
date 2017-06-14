7cf63dd6bbfc677122def6931c8a9e858f521a6d
Administrator <541084008@qq>
2017-06-14 14:52:50 +0800

    hw

--- src/huawei/h_3.java
+++ src/huawei/h_3.java
@@ -1,28 +1,70 @@
 package huawei;
 
+import java.util.Scanner;
+
 /**
  * Created by Administrator on 2017/6/6.
- * 有这样一道智力题：
- * “某商店规定：三个空汽水瓶可以换一瓶汽水。
- * 小张手上有十个空汽水瓶，她最多可以换多少瓶汽水喝？”
- * 答案是5瓶，方法如下：先用9个空瓶子换3瓶汽水，喝掉3瓶满的，
- * 喝完以后4个空瓶子，用3个再换一瓶，喝掉这瓶满的，这时候剩2个空瓶子。
- * 然后你让老板先借给你一瓶汽水，喝掉这瓶满的，
- * 喝完以后用3个空瓶子换一瓶满的还给老板。
- * 如果小张手上有n个空汽水瓶，最多可以换多少瓶汽水喝？
- 输入描述:
- 输入文件最多包含10组测试数据，每个数据占一行，仅包含一个正整数n（1<=n<=100），表示小张手上的空汽水瓶数。n=0表示输入结束，你的程序不应当处理这一行。
- 输出描述:
- 对于每组测试数据，输出一行，表示最多可以喝的汽水瓶数。如果一瓶也喝不到，输出0。
- 输入例子:
- 3
- 10
- 81
- 0
- 输出例子:
- 1
- 5
- 40
+ * 出一个程序，接受一个十六进制的数值字符串，输出该数值的十进制字符串。（多组同时输入）
+ * 输入描述:
+ * 输入一个十六进制的数值字符串。
+ * 输出描述:
+ * 输出该数值的十进制字符串。
+ * 输入例子:
+ * 0xA
+ * 输出例子:
+ * 10
  */
 public class h_3 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while (in.hasNext()) {
+            String str = in.nextLine().substring(2);
+            System.out.println(Integer.parseInt(str, 16));
+        }
+    }
+}
+
+//Method 2
+class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNext()) {
+
+            StringBuffer sb = new StringBuffer();
+
+            sb.append(sc.next());
+
+            //String str=sc.next();   
+
+            String str = sb.reverse().substring(0, sb.length() - 2);
+
+            char ch[] = str.toCharArray();
+
+            int sum = 0;
+
+            for (int i = 0; i < ch.length; i++) {
+
+                if (ch[i] >= 'A' && ch[i] <= 'F') {
+
+
+                    sum += (Integer.valueOf(ch[i]) - 55) * Math.pow(16, i);
+
+                } else {
+
+
+                    sum += (Integer.valueOf(ch[i]) - 48) * Math.pow(16, i);
+
+                }
+
+            }
+
+            System.out.println(sum);
+
+            // System.out.println(Integer.valueOf('6'));
+
+        }
+
+    }
 }
