bb97ab8a3f600b621f77726caf34e4855b41cfb5
Administrator <541084008@qq>
2017-06-20 10:01:06 +0800

    leetcoleetcode

---
+++ src/leetcode/l1.java
@@ -0,0 +1,46 @@
+/**
+ * Created by Administrator on 2017/6/20 0020.
+ * Given a binary tree, find its minimum depth.
+ * The minimum depth is the number of nodes along the shortest
+ * path from the root node down to the nearest leaf node.
+ */
+package leetcode;
+import java.util.LinkedList;
+import java.util.Queue;
+public class l1 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public int run(TreeNode root) {
+        Queue<TreeNode> stack = new LinkedList<TreeNode>();
+        int minDepth = 1;
+        if (root == null) {
+            return 0;
+        }
+        stack.add(root);
+        while (!stack.isEmpty()) {
+            int len = stack.size();
+            for (int i = 0; i < len; i++) {
+            root = stack.poll();
+            if (root.right == null && root.left == null) {
+                return minDepth;
+            }
+            if (root.left != null) {
+                stack.add(root.left);
+            }
+            if (root.right != null) {
+                stack.add(root.right);
+            }
+            }
+            minDepth++;
+        }
+        return minDepth;
+    }
+}
---
+++ src/leetcode/l2.java
@@ -0,0 +1,46 @@
+package leetcode;
+
+
+/**
+ * Created by Administrator on 2017/6/20 0020.
+ * Evaluate the value of an arithmetic expression in Reverse Polish Notation.
+ * Valid operators are+,-,*,/. Each operand may be an integer or another expression.
+ * Some examples:
+ * ["2", "1", "+", "3", "*"] -> ((2 + 1) * 3) -> 9
+ * ["4", "13", "5", "/", "+"] -> (4 + (13 / 5)) -> 6
+ * 值的比较还是equal是最靠谱的
+// */
+import java.util.Stack;
+public class l2 {
+    public static int evalRPN(String[] tokens) {
+        Stack<String> stack = new Stack<String>();
+        int len = tokens.length;
+        for (int i = 0; i < len; i++) {
+            if (tokens[i].equals("*")|| tokens[i].equals("+")|| tokens[i].equals("-")|| tokens[i].equals("/")) {
+                String b = stack.pop();
+                String a = stack.pop();
+                int aNum = Integer.parseInt(a);
+                int bNum = Integer.parseInt(b);
+                if (tokens[i].equals("+")) {
+                    int sum = aNum + bNum;
+                    stack.push(String.valueOf( sum));
+                } else if (tokens[i].equals("-")) {
+                    int sum = aNum - bNum;
+                    stack.push(String.valueOf( sum));
+                } else if (tokens[i].equals( "/") ){
+                    int sum = aNum/bNum;
+                    stack.push(String.valueOf( sum));
+                } else if (tokens[i].equals("*")) {
+                    int sum = aNum * bNum;
+                    stack.push(String.valueOf( sum));
+                }
+            }else { stack.push(tokens[i]);}
+        }
+        String result = stack.pop();
+        return Integer.parseInt(result);
+    }
+    public static void main(String[] args)
+    {
+        evalRPN(new String[]{"0","3","/"});
+    }
+}
