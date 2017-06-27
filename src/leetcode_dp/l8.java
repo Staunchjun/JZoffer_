4ab92d4d90a3985281c92820ec38f3b76afda781
Administrator <541084008@qq>
2017-06-27 13:38:56 +0800

    leetcoleetcode

--- src/leetcode_dp/l8.java
+++ src/leetcode_dp/l8.java
@@ -1,7 +1,27 @@
 package leetcode_dp;
+import java.util.Set;
 
 /**
  * Created by Administrator on 2017/6/27 0027.
+ Given a string s and a dictionary of words dict, determine
+ if s can be segmented into a space-separated sequence of one or more dictionary words.
+ For example, given
+ s ="leetcode",
+ dict =["leet", "code"].
+ Return true because"leetcode"can be segmented as"leet code".
  */
 public class l8 {
+    public boolean wordBreak(String s, Set<String> dict) {
+        int len = s.length();
+        boolean[] dp = new boolean[len+1];
+        dp[0] = true;
+        for (int i = 1; i <len+1 ; i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j]&&dict.contains(s.substring(j,i)))
+                { dp[i] = true;
+                break;}
+            }
+        }
+        return dp[len];
+   }
 }
