b93ce33f9a8eb1c842d0c401f0b1aaa641b7b8e5
Staunchjun <Staunchjun@users.noreply.github.com>
2020-01-06 19:21:24 +0800

    Update LeetCode287.java

--- src/leetcode/LeetCode287.java
+++ src/leetcode/LeetCode287.java
@@ -1,4 +1,21 @@
 package leetcode;
 
 public class LeetCode287 {
+        int left = 1;
+        int right = nums.length;
+         while (left < right) {
+            int mid = (left + right) >>> 1;
+            int count = 0;
+            for(int num : nums){
+                if(num <= mid){
+                    count += 1;
+                }
+            }
+            if(count > mid){
+                right = mid;
+            }else{
+                left = mid + 1;
+            }
+        }
+        return left;
 }
