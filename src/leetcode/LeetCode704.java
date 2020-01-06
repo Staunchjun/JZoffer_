fdc13638e3f7ab04de654cbd377af2ecec9b2e33
Staunchjun <Staunchjun@users.noreply.github.com>
2020-01-06 19:51:29 +0800

    Update LeetCode704.java

--- src/leetcode/LeetCode704.java
+++ src/leetcode/LeetCode704.java
@@ -1,4 +1,17 @@
 package leetcode;
 
 public class LeetCode704 {
+         int mid, left = 0, right = nums.length - 1;
+            while (left <= right) {
+                 mid = (left + right) >>> 1;
+                 if (nums[mid] == target) {
+                        return mid;
+                 }
+                 if (target > nums[mid]) {
+                        left = mid+ 1;
+                 }else{
+                        right = mid - 1 ;
+                 } 
+            }
+        return -1;
 }
