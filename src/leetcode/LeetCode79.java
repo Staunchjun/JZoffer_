5e5a47e96b04bfdaebea9fe27fd6fc7b7bde6279
staunchjun <541084008@qq.com>
2020-02-01 21:36:12 +0800

    DFS Leetcode 入门 79题

--- src/leetcode/LeetCode79.java
+++ src/leetcode/LeetCode79.java
@@ -2,15 +2,14 @@ package leetcode;
 
 public class LeetCode79 {
     private boolean[][] marked;
-
     //        x-1,y
     // x,y-1  x,y    x,y+1
     //        x+1,y
-    private int[][] direction = {{-1, 0}, {0, -1}, {0, 1}, {1, 0}};
+    private int[][] directions = {{-1, 0}, {0, -1}, {0, 1}, {1, 0}};
     // 盘面上有多少行
     private int m;
     // 盘面上有多少列
-    private int n;
+    private int col;
     private String word;
     private char[][] board;
 
@@ -19,13 +18,13 @@ public class LeetCode79 {
         if (m == 0) {
             return false;
         }
-        n = board[0].length;
-        marked = new boolean[m][n];
+        col = board[0].length;
+        marked = new boolean[m][col];
         this.word = word;
         this.board = board;
 
         for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
+            for (int j = 0; j < col; j++) {
                 if (dfs(i, j, 0)) {
                     return true;
                 }
@@ -35,7 +34,55 @@ public class LeetCode79 {
     }
 
     private boolean dfs(int i, int j, int start) {
+        /**
+         * 先写退出的条件吧
+         * 当索引等于单词长度的时候，表示找完了所有的，可以返回
+         * 这里到了最后一个需要判断下是否相等，既然都走到这里了
+         * 最后一个相等就OK啦
+         */
+        if (start == word.length() - 1){
+            return board[i][j] == word.charAt(start);
+        }
 
+        /**
+         * 判断是否和word里的字母一样
+         */
+        if (board[i][j] == word.charAt(start)){
+            /**
+             * 标志一下，已经走过了
+             */
+            marked[i][j] = true;
+            /**
+             * 上下左右地走呗
+             */
+            for (int[] direction:directions) {
+                int x = i+direction[0];
+                int y = j+direction[1];
+                /**
+                 * 注意边界啊, 以及判断下有没有走过
+                 */
+                if (inArea(x,y) && !marked[x][y]){
+                    /**
+                     * 这里别用++start,如果用了++start会影响别的
+                     *  好好用start+1不行吗，非要对start自增？
+                     */
+                    if (dfs( x,  y,   start+1 )){
+                        return true;
+                    }
+                }
+            }
+
+            /**
+             * 恢复下标志位，还记得回溯吗23333,就是排列组合
+             * 第一种你试过了，总要恢复下标志位吧
+             */
+            marked[i][j] = false;
+        }
+        return false;
+    }
+
+    private boolean inArea(int i,int j){
+        return i < m && j < col && i >= 0 && j >= 0;
     }
 
     public static void main(String[] args) {
---
+++ src/leetcode/LeetCode807.java
@@ -0,0 +1,12 @@
+package leetcode;
+
+public class LeetCode807 {
+    public int maxIncreaseKeepingSkyline(int[][] grid) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        LeetCode807 leetCode807 = new LeetCode807();
+
+    }
+}
